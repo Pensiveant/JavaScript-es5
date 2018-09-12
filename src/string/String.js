f6f593b75aa3ad351286780093989fb7882b40ba
ZXW <1429055703@qq.com>
2018-09-12 09:23:24 +0800

    string总结

---
+++ src/string/String.js
@@ -0,0 +1,17 @@
+var chartStr = "abcdefghijklmnopqrstuvwxyz";
+
+
+//#region 方法
+console.log(chartStr.slice(1, 3));//adc
+console.log(chartStr.substr(0, 3));//adc
+//不包括结尾位置
+console.log(chartStr.substring(3, 7));//defg
+//#endregion
+
+//#region 静态方法
+//#endregion
+
+
+//#region string中html方法
+
+//#endregion
---
+++ src/string/stringOporate.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+//判断单词是否回文
+//思路：将字符串转换为数组，并使用数组的reverse()将字符串反转，在转为字符串进行比较。
+function checkPlaindrom(str){
+    return str==str.split("").reverse().join("");
+}
