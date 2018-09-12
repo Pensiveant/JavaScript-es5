a3d71fd258de5aaad1128e4d4e155ffad809e49b
ZXW <1429055703@qq.com>
2018-09-12 09:27:26 +0800

    index修改

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // var type = require("./type/typeof");
 // var createSingleObject = require("./object/createSingleObject.js");
-require("./object/objectAttributeOprate");
-
-
-
+// require("./object/objectAttributeOprate");
+//  require("./object/objectAttribute");
+// require("./function/closures");
+require("./string/String");
 
 var p = document.createElement("p");
 p.innerHTML = "hello es5!";
